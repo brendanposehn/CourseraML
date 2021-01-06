5e3d1fc077ab6050b696b4dba1443d82443f7714
brendanposehn <brendanposehn0@gmail.com>
2021-01-06 10:05:55 -0800

    Finished Ex 5

--- ex5/learningCurve.m
+++ ex5/learningCurve.m
@@ -17,6 +17,9 @@ function [error_train, error_val] = ...
 % Number of training examples
 m = size(X, 1);
 
+m_train =  size(X, 1);
+m_val = size(Xval, 1);
+
 % You need to return these values correctly
 error_train = zeros(m, 1);
 error_val   = zeros(m, 1);
@@ -53,11 +56,15 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
+% linearRegCostFunction(X, y, theta, lambda)
+% [theta] = trainLinearReg(X, y, lambda)
 
-
-
-
-
+for i=1:m
+    theta = trainLinearReg(X(1:i, :), y(1:i), lambda); %same size no matter how many examples we give
+       
+    [error_train(i), ~] = linearRegCostFunction(X(1:i, :), y(1:i), theta, 0);    
+    [error_val(i), ~] = linearRegCostFunction(Xval, yval, theta, 0);
+end
 
 % -------------------------------------------------------------
 
--- ex5/linearRegCostFunction.m
+++ ex5/linearRegCostFunction.m
@@ -19,14 +19,10 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
+J = (1/(2*m))*sum((X*theta - y).^2) + (lambda/(2*m))*(theta(2:end)'*theta(2:end));
 
-
-
-
-
-
-
-
+grad = (1/m)*X'*(X*theta - y) + (lambda/m)*theta;
+grad(1) = grad(1) - (lambda/m)*theta(1);
 
 
 
